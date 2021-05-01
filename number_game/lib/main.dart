5f7a683e80d39f895a0ceedbed3ffa67ab1edb09
taketo43 <take10.43.aki@gmail.com>
2021-05-01 16:04:15 +0900

    トップページにサーバー処理記載

--- number_game/lib/main.dart
+++ number_game/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:number_game/created_game_list.dart';
 import 'package:number_game/waiting_room.dart';
+import 'package:number_game/socket_io_setup.dart';
 
 void main() {
   runApp(MyApp());
@@ -37,74 +38,83 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    String roomId = 'test';
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'User Name',
-              style: TextStyle(
-                fontSize: 13,
+      body: StreamBuilder(
+          stream: streamSocket.getResponse,
+          builder: (BuildContext context,
+              AsyncSnapshot<Map<String, dynamic>> snapshot) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'User Name',
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                  // Text(
+                  //   host,
+                  //   style: TextStyle(
+                  //     fontSize: 25,
+                  //   ),
+                  // ),
+                  Container(
+                    width: 150,
+                    child: new TextField(
+                      textAlign: TextAlign.center,
+                      maxLength: 15,
+                      maxLengthEnforced: false,
+                      maxLines: 1,
+                      onChanged: _handleText,
+                    ),
+                  ),
+                  Container(
+                    height: 30,
+                  ),
+                  ButtonTheme(
+                    buttonColor: Colors.white54,
+                    minWidth: 150,
+                    height: 40,
+                    child: RaisedButton(
+                        child: Text("ホストで始める"),
+                        onPressed: () {
+                          socket.emit('create', [roomId, username]);
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => WaitingRoom(username)));
+                          //　サーバーに名前を渡す？
+                        }),
+                  ),
+                  Container(
+                    height: 20,
+                  ),
+                  ButtonTheme(
+                    minWidth: 150,
+                    height: 40,
+                    buttonColor: Colors.white54,
+                    child: RaisedButton(
+                        child: Text("ゲームに参加する"),
+                        onPressed: () {
+                          socket.emit('get rooms');
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    CreatedGameList(username)),
+                          );
+                          // サーバーに名前を渡す？
+                        }),
+                  )
+                ],
               ),
-            ),
-            // Text(
-            //   host,
-            //   style: TextStyle(
-            //     fontSize: 25,
-            //   ),
-            // ),
-            Container(
-              width: 150,
-              child: new TextField(
-                textAlign: TextAlign.center,
-                maxLength: 15,
-                maxLengthEnforced: false,
-                maxLines: 1,
-                onChanged: _handleText,
-              ),
-            ),
-            Container(
-              height: 30,
-            ),
-            ButtonTheme(
-              buttonColor: Colors.white54,
-              minWidth: 150,
-              height: 40,
-              child: RaisedButton(
-                  child: Text("ホストで始める"),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => WaitingRoom(username)));
-                    //　サーバーに名前を渡す？
-                  }),
-            ),
-            Container(
-              height: 20,
-            ),
-            ButtonTheme(
-              minWidth: 150,
-              height: 40,
-              buttonColor: Colors.white54,
-              child: RaisedButton(
-                  child: Text("ゲームに参加する"),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => CreatedGameList(username)),
-                    );
-                    // サーバーに名前を渡す？
-                  }),
-            )
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            );
+          }), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
